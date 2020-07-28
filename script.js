77a40b88006b6a9663fcc9a6b181ea8f7b890d32
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-28 10:58:59 +0600

    day, month, year added

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function startingTime() {
     let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth();
+    let year = today.getFullYear();
     let hours = today.getHours();
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
     minutes = addZero(minutes);
     seconds = addZero(seconds);
+    document.getElementById('date').innerHTML = day + "-" + month + "-" + year;
     document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds;
     setTimeout(startingTime, 0);
 }
